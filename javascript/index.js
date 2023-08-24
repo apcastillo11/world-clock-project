7793013ccdcb329839ce884fe4b91da67178e0e5
a2dael2dawhy <55224339+apcastillo11@users.noreply.github.com>
2023-08-23 20:31:25 -0700

    Added moment time zones

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let amsterdamElement = document.querySelector("#amsterdam");
+  let amsterdamDateElement = amsterdamElement.querySelector(".date");
+  let amsterdamTimeElement = amsterdamElement.querySelector(".time");
+  let amsterdamTime = moment().tz("Europe/Amsterdam");
+  amsterdamDateElement.innerHTML = amsterdamTime.format("dddd, MMMM Do, YYYY");
+  amsterdamTimeElement.innerHTML = amsterdamTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  let buenosAiresElement = document.querySelector("#buenos-aires");
+  let buenosAiresDateElement = buenosAiresElement.querySelector(".date");
+  let buenosAiresTimeElement = buenosAiresElement.querySelector(".time");
+  let buenosAiresTime = moment().tz("America/Buenos_Aires");
+  buenosAiresDateElement.innerHTML = buenosAiresTime.format(
+    "dddd, MMMM Do, YYYY"
+  );
+  buenosAiresTimeElement.innerHTML = buenosAiresTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
